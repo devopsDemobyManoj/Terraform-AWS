0b30f41b1549eec86b8699a2821d1a557e8de176
devopspundit <67195224+devops-pundit@users.noreply.github.com>
2023-06-20 07:48:13 -0500

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-provider "aws" {
-    region = "us-east-2"  
-}
-
-resource "aws_instance" "ec2" {
-  ami           = "ami-0e820afa569e84cc1" #us-east-2
-  instance_type = "t2.micro"
-  tags = {
-      Name = "Terraform-Demo"
-  }
-}
+module "ec2" {
+  source = "./modules/ec2"
+ }
