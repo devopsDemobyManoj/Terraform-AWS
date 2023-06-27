70ac8a993eb2a071cee03e0d6bd1753e0b8e8b21
devopspundit <67195224+devops-pundit@users.noreply.github.com>
2023-06-26 19:42:50 -0500

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2" {
   ami           = "ami-0e820afa569e84cc1" #us-east-2
   instance_type = "t2.micro"
   tags = {
-      Name = "Terraform-Demo"
+      Name = "Terraform-Ansible"
   }
   key_name = "jenkinsDemo"
   security_groups = "sg-0d7515bee459b60fc"
