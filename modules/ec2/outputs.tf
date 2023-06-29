c6811ba9449921d65b7f520b99ac9d1aaec90eca
devopspundit <67195224+devops-pundit@users.noreply.github.com>
2023-06-28 23:47:16 -0500

    Update outputs.tf

--- modules/ec2/outputs.tf
+++ modules/ec2/outputs.tf
@@ -1,4 +1,3 @@
 output "public_ip" {
-  description = "Copy/Paste/Enter - You are in the matrix"
-  value       = "ssh -i ${module.ssh-key.key_name}.pem ec2-user@${module.ec2.public_ip}"
+  value = aws_instance.ec2_public.public_ip
 }
