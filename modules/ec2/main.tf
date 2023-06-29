964f326f9b95b382f45cfd57534ad03e52dbe3ae
devopspundit <67195224+devops-pundit@users.noreply.github.com>
2023-06-29 00:33:31 -0500

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "ec2" {
   ami           = "ami-0e820afa569e84cc1" #us-east-2
   instance_type = "t2.micro"
   tags = {
-      Name = "Terraform-Ansible"
+      Name = "Terraform-Ansible-docker"
   }
   key_name = "jenkinsDemo"
   security_groups = ["launch-wizard-2"]
